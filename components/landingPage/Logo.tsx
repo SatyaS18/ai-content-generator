1f8288f5715e2fa63b63716001bbaf36b017a061
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-10-02 05:29:37 +0530

    add logo

---
+++ components/landingPage/Logo.tsx
@@ -0,0 +1,11 @@
+function Logo() {
+  return (
+    <div className="logo">
+      <h2 className="text-3xl font-bold text-gray-900">
+        Forge<span className="text-[#6941c6]">Craft</span>
+      </h2>
+    </div>
+  );
+}
+
+export default Logo;
