c72f69288ce7b462e4e3f80585d4695080fea34c
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-10-02 05:14:32 +0530

    add header component

---
+++ app/dashboard/_components/Header.tsx
@@ -0,0 +1,29 @@
+import { UserButton } from "@clerk/clerk-react";
+import { Search, MenuSquare } from "lucide-react";
+
+function Header({ toggleSidebar }: { toggleSidebar: () => void }) {
+  return (
+    <div className="p-5 shadow-sm border-b-2 flex justify-between items-center bg-white">
+      <div className="flex gap-2 items-center p-2 border rounded-md max-w-lg bg-white">
+        <Search />
+        <input type="text" placeholder="Search..." className="outline-none" />
+      </div>
+      <div className="flex gap-5 items-center">
+        <h2 className="bg-primary lg:block hidden p-1 rounded-full text-xs px-2 text-orange-400">
+          ️‍🔥{" "}
+          <span className="text-white">
+            Join Membership for just $9.99/Month
+          </span>
+        </h2>
+        <div className="lg:block hidden">
+          <UserButton />
+        </div>
+        <div className="md:hidden block" onClick={toggleSidebar}>
+          <MenuSquare className="w-6 h-6 text-primary" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
