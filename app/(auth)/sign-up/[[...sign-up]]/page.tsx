44b3312eb8687ff437c0b17af30dea3c47d29793
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-10-02 05:06:08 +0530

    add sign-up and sign-in page

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+// import Footer from "@/components/landingPage/Footer";
+// import Navbar from "@/components/landingPage/Navbar";
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <div className="flex item-center justify-center h-screen mt-20">
+        <SignUp />
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+}
