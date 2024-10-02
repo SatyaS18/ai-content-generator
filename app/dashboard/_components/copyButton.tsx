5f652ce97d5b23bad2b5152e4fde727eed6f40a5
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-10-03 03:11:44 +0530

    add copy button

---
+++ app/dashboard/_components/copyButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+
+interface CopyButtonProps {
+  textToCopy: string;
+}
+
+const CopyButton: React.FC<CopyButtonProps> = ({ textToCopy }) => {
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(textToCopy);
+      alert("AI Response copied to clipboard!");
+    } catch (err) {
+      alert("Failed to copy text: " + err);
+    }
+  };
+
+  return (
+    <button
+      className="ml-5 p-2 text-blue-500 text-sm bg-blue-50 rounded hover:text-blue-600 hover:font-medium transition-all"
+      onClick={copyToClipboard}
+    >
+      Copy
+    </button>
+  );
+};
+
+export default CopyButton;
