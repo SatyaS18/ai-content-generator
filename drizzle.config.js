f69c634f4b28e51651212635c99513cef2609cb6
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-10-03 01:23:09 +0530

    setup schema file and url

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,8 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-  schema: "./schema.ts",
+  schema: "./utils/schema.tsx",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DB_URL,
+    url: "postgresql://test_owner:A4LFQyl7hIdE@ep-soft-dust-a5vtjh15.us-east-2.aws.neon.tech/ai-content-generator?sslmode=require",
   },
 };
