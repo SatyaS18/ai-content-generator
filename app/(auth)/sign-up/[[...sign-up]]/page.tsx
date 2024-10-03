f08b9634d8de5469bc5d3fd6f68ff78e926e8264
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-10-04 02:25:11 +0530

    add navbar and footer component

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,15 +1,15 @@
-// import Footer from "@/components/landingPage/Footer";
-// import Navbar from "@/components/landingPage/Navbar";
+import Footer from "@/components/landingPage/Footer";
+import Navbar from "@/components/landingPage/Navbar";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <div className="flex item-center justify-center h-screen mt-20">
         <SignUp />
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
