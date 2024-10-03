c90d18a057b7157fd1ca801e491b09b1ceaa94f8
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-10-04 01:43:33 +0530

    add context wrapper

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,6 +2,9 @@
 import { useState } from "react";
 import SideNav from "./_components/SideNav";
 import Header from "./_components/Header";
+import { TotalUsageContext } from "../(context)/TotalUsageContext";
+import { UserSubscriptionContext } from "../(context)/UserSubscriptionContext";
+import { UpdateCreditUsageContext } from "../(context)/UpdateCreditUsageContext";
 
 function Layout({
   children,
@@ -9,20 +12,31 @@ function Layout({
   children: React.ReactNode;
 }>) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [totalUsage, setTotalUsage] = useState<number>(0);
+  const [totalUsage, setTotalUsage] = useState<any>(0);
   const [userSubscription, setUserSubscription] = useState<boolean>(false);
+  const [updateCreditUsage, setUpdateCreditUsage] = useState<any>();
 
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
   return (
-    <div className="bg-slate-100 h-screen poppins">
-      <div className="md:w-64 fixed">
-        <SideNav isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      </div>
-      <div className="md:ml-64">
-        <Header toggleSidebar={toggleSidebar} />
-        {children}
-      </div>
-    </div>
+    <TotalUsageContext.Provider value={{ totalUsage, setTotalUsage }}>
+      <UserSubscriptionContext.Provider
+        value={{ userSubscription, setUserSubscription }}
+      >
+        <UpdateCreditUsageContext.Provider
+          value={{ updateCreditUsage, setUpdateCreditUsage }}
+        >
+          <div className="bg-slate-100 h-screen poppins">
+            <div className="md:w-64 fixed">
+              <SideNav isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+            </div>
+            <div className="md:ml-64">
+              <Header toggleSidebar={toggleSidebar} />
+              {children}
+            </div>
+          </div>
+        </UpdateCreditUsageContext.Provider>
+      </UserSubscriptionContext.Provider>
+    </TotalUsageContext.Provider>
   );
 }
 
