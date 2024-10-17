24b6660d1701572e893161273305c1266abfc46e
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-10-17 07:24:10 +0530

    added routing

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Settings() {
   return (
     <div className="flex items-center justify-center my-5">
-      <UserProfile />
+      <UserProfile routing="hash" />
     </div>
   );
 }
