90200b1a56b5c525deb3f8836438e576e5c6e119
SATYAKANT SAHU <imt_2020088@iiitm.ac.in>
2024-11-17 00:20:40 +0530

    update db url

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   schema: "./utils/schema.tsx",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://test_owner:A4LFQyl7hIdE@ep-soft-dust-a5vtjh15.us-east-2.aws.neon.tech/ai-content-generator?sslmode=require",
+    url: process.env.DB_URL,
   },
 };
